31254a1789422a15a6de068e11894787855045db
Jennifer Leclaire <leclaire@uni-mainz.de>
2022-06-22 15:08:14 +0200

    [test] created new structure for testing purposes based on tk/ImplicitArray.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,36 +1,8 @@
 using BiochemicalAlgorithms
 using Test
 
-function test_pubchem()
-    @testset "PubChem" begin
-        mol = load_pubchem_json("./data/aspirin_pug.json")
-
-        @test mol.name == "./data/aspirin_pug.json"
-
-        @test count_atoms(mol) == 21
-        @test count_bonds(mol) == 21
-    end
-end
-
-function test_pdb()
-    @testset "PDB" begin
-        bpti = load_pdb("./data/bpti.pdb")
-
-        @test bpti.name == "bpti.pdb"
-        
-        @test count_atoms(bpti) == 454
-        @test count_bonds(bpti) == 0
-
-        pdb_5pti = load_pdb("./data/5PTI.pdb")
-
-        @test pdb_5pti.name == "5PTI.pdb"
-
-        @test count_atoms(pdb_5pti) == 1087
-        @test count_bonds(pdb_5pti) == 0
-    end
-end
-
 @testset "BiochemicalAlgorithms.jl" begin
-    test_pdb()
-    test_pubchem()
+   
+    @testset "Fileformats" begin include("test_fileformats.jl") end
+   
 end
