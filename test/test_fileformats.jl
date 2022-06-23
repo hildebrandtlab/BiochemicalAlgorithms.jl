e0808305f66224f832f8cc129e9df179e8362d48
Jenny@zovirax <leclaire@uni-mainz.de>
2022-06-23 10:28:22 +0200

    [fileformats]fixed wrong paths in testing files

--- test/test_fileformats.jl
+++ test/test_fileformats.jl
@@ -1,12 +1,12 @@
 @testset "PDB" begin
-        bpti = load_pdb("test/data/bpti.pdb")
+        bpti = load_pdb("data/bpti.pdb")
 
         @test bpti.name == "bpti.pdb"
 
         @test count_atoms(bpti) == 454
         @test count_bonds(bpti) == 0
 
-        pdb_5pti = load_pdb("test/data/5PTI.pdb")
+        pdb_5pti = load_pdb("data/5PTI.pdb")
 
         @test pdb_5pti.name == "5PTI.pdb"
 
@@ -16,9 +16,9 @@
 end
 
 @testset "PubChem" begin
-        mol = load_pubchem_json("test/data/aspirin_pug.json")
+        mol = load_pubchem_json("data/aspirin_pug.json")
         
-        @test mol.name == "test/data/aspirin_pug.json"
+        @test mol.name == "data/aspirin_pug.json"
 
         @test count_atoms(mol) == 21
         @test count_bonds(mol) == 21
