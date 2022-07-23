174f3b88e1162a28e6ac4d50ac40c7992c61fc1a
shuettel <shuettel@students.uni-mainz.de>
2022-07-23 17:06:17 +0200

    antechamber data and dataframe beginning

--- src/core/atomtyping.jl
+++ src/core/atomtyping.jl
@@ -1,24 +1,48 @@
 
 using BiochemicalAlgorithms: Molecule, Bond, Atom
-using DataFrames
+using DataFrames, DelimitedFiles, CSV
 
 export Atomtype, make_dict_from_dat
 
-function atomtype_gaff(num::UInt128)
-    atom_gaff = BiochemicalAlgorithms.Molecule.atoms[num]
-    # bonds_gaff = subset(groupby(BiochemicalAlgorithms.Molecule.bonds, :order), :aid2 => aid1 -> aid1 = num)
-    bonds_gaff = subset(BiochemicalAlgorithms.Molecule.bonds, aid1 == num)
+function atomtype_gaff(num::UInt128, mol::Molecule)
+    atom_gaff = mol.atoms[num]
+    bonds_gaff = subset(mol.bonds, aid1 == num)
     
     for i = 1:length(max(Molecule.bonds.a1))
-    
+    end
 end
 
-function make_dict_from_dat(mapfile::AbstractString, firstline::UInt16, lastline::UInt16)
-    dat_file = open(mapfile)
-    Dat_dict = Dict{String, UInt16}()
-    for (i, line) in enumerate(eachline(dat_file))
-        if (i >= firstline & i <= lastline)
-            Dat_dict(line[1:2] => i)
+function remove_space_add_comma(line::AbstractString)
+    out_str = ""
+    i = 1
+	while i <= lastindex(line)
+        if i > 1 && line[i-1] == ' ' && line[i] != ' ' && line[i] != '&'
+                out_str = string(out_str,",")
+        end
+        if line[i] != ' ' && line[i] != '&' && line[i] != " "
+            out_str = string(out_str,line[i])
+        end
+        if i == lastindex(line)
+            out_str = string(out_str,'&')
+        end
+        i += 1
+	end
+    return out_str
+end
+
+function df_maker(mapfile::AbstractString)
+    source_file = open(mapfile)
+    temp_str = ""
+    temp_arr = []
+    for line in readlines(source_file)
+        if lastindex(line)>=3 && line[1:3] == "ATD"
+            temp_var = remove_space_add_comma(line)
+            temp_str = string(temp_str, temp_var)
+            append!(temp_arr,temp_str)            
         end
     end
+    # temp_str = readdlm(temp_str, ',', AbstractString, '&')
+    writedlm("src\\mappings\\antechamber\\atomtype_gaff.csv", temp_arr, ',')
+    df_atomtype = CSV.read("src\\mappings\\antechamber\\atomtype_gaff.csv")
+    return df_atomtype
 end
