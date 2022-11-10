66e85ed6b75194ba280cd15abe2c3417080fb6b5
shuettel7 <shuettel@students.uni-mainz.de>
2022-11-10 09:26:09 +0100

    Auto stash before merge of "shuettel7/atomtyping" and "origin/shuettel7/atomtyping"

--- src/validation/ball_atomtyping_validation/main.cpp
+++ src/validation/ball_atomtyping_validation/main.cpp
@@ -1,6 +1,8 @@
 #include <BALL/KERNEL/system.h>
 #include <BALL/FORMAT/PDBFile.h>
 #include <BALL/FORMAT/MOL2File.h>
+#include <BALL/MOLMEC/AMBER/GAFFTypeProcessor.h>
+#include <BALL/STRUCTURE/assignBondOrderProcessor.h>
 
 #include <BALL/FORMAT/genericMolFile.h>
 #include <BALL/FORMAT/molFileFactory.h>
@@ -17,6 +19,20 @@ int main(int argc, char** argv) {
 		cerr << "Need one filename! Aborting." << endl;
 		return(1);
 	}
+
+    GenericMolFile* infile = MolFileFactory::open(argv[1]);
+
+	if (!infile)
+	{
+		Log.error() << "Could not determine filetype, aborting" << std::endl;
+		return 2;
+	}
+
+	if (!*infile)
+	{
+		std::cerr << "Invalid file, aborting" << std::endl;
+		return 2;
+	}
 	
 	String infile_name(argv[1]); // ("../../../../../huettel-msc/export_folder/gaff_files/1_1,3-Butadien_Conformer3D_CID_7845.mol2"); 
 	String outfile_name(infile_name);
@@ -25,17 +41,32 @@ int main(int argc, char** argv) {
 	outfile_name.reverse();
 	outfile_name = outfile_name + "out.mol2";
 
-    MOL2File f(infile_name);
+    // MOL2File f(infile_name);
     System S;
-    f >> S;
+    *infile >> S;
+
+    Options options;
+    options[GAFFTypeProcessor::Option::ATOMTYPE_FILENAME] = "atomtyping/GAFFTypes.dat";
+
+    GAFFTypeProcessor gt(options);
+    S.apply(gt);
+
+    for (AtomIterator at_it = S.beginAtom(); +at_it; ++at_it)
+		std::cout << "atom name: " << at_it->getName() << " atomtype  " << at_it->getProperty("atomtype").getString() << std::endl;
+
+    AssignBondOrderProcessor abp;
+	S.apply(abp);
 
     cout << "System contains " << S.countAtoms() << " atoms." << endl;
     
+	GenericMolFile* outfile = MolFileFactory::open(outfile_name, std::ios::out);
+    *outfile << S;
+    outfile->close();
 
-	MOL2File mol2_file(outfile_name, ios::out);
-	mol2_file << S;
-	mol2_file.close();
-	cout << "Wrote MOL2 file " << outfile_name << endl;
+	// MOL2File mol2_file(outfile_name, ios::out);
+	// mol2_file << S;
+	// mol2_file.close();
+	// cout << "Wrote MOL2 file " << outfile_name << endl;
 
     return 0;
 }
