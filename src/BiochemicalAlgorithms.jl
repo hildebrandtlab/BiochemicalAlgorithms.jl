c214dc6b69ff7b3ddedd23636584a61d4b6a0c52
Thomas Kemmer <thomas@binaryden.de>
2023-01-13 14:20:14 +0100

    Unify naming scheme for source files (fixes #31)
    Capitalized file names shall only be used for the main module.
    
    Signed-off-by: Thomas Kemmer <thomas@binaryden.de>

--- src/BiochemicalAlgorithms.jl
+++ src/BiochemicalAlgorithms.jl
@@ -4,7 +4,7 @@ include("core/types.jl")
 include("core/element.jl")
 include("core/amino_acid.jl")
 include("core/atom.jl")
-include("core/PDB_atom.jl")
+include("core/pdb_atom.jl")
 include("core/bond.jl")
 include("core/chain.jl")
 include("core/molecule.jl")
@@ -17,7 +17,7 @@ include("core/protein.jl")
 module PubChem
 include("fileformats/pubchem_json.jl")
 end
-include("fileformats/PDB.jl")
+include("fileformats/pdb.jl")
 
 include("mappings/atom_bijection.jl")
 include("mappings/rigid_mapping.jl")
