3ddbb97cf94a81e6070961d699d5b8cf24a5eaca
Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>
2023-02-14 16:57:49 +0100

    Add has_, get_, and set_property to Atom
    Signed-off-by: Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>

--- src/core/atom.jl
+++ src/core/atom.jl
@@ -1,4 +1,4 @@
-export Atom
+export Atom, has_property, get_property, set_property
 
 const Atom{T} = @NamedTuple begin
     number::Int
@@ -12,4 +12,16 @@ const Atom{T} = @NamedTuple begin
     has_force::Bool
     frame_id::Int
     properties::Properties
+end
+
+function has_property(a::Atom, key::String)
+    haskey(a.properties, key)
+end
+
+function get_property(a::Atom, key::String)
+    a.properties[key]
+end
+
+function set_property(a::Atom, key::String, value)
+    a.properties[key] = value
 end
