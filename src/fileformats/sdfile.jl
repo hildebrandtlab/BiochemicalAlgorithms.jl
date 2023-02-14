f4edc296a7471f98a2cb9c0427f473debb3718dc
Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>
2023-02-14 15:50:09 +0100

    Add write functionality for sd files
    Signed-off-by: Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>

--- src/fileformats/sdfile.jl
+++ src/fileformats/sdfile.jl
@@ -1,9 +1,22 @@
-export load_sdfile
+export load_sdfile, write_sdfile
 
-using MolecularGraph: sdfilereader
+using MolecularGraph: sdfilereader, sdfilewriter,
+    GraphMol, SDFileAtom, SDFileBond
 
 function load_sdfile(fname::String, T=Float32)
     mg_mols = [m for m in sdfilereader(fname)]
 
     [convert(Molecule{T}, m) for m in mg_mols]
+end
+
+function write_sdfile(fname::String, mol::AbstractMolecule)
+    mg_mol = convert(GraphMol{SDFileAtom, SDFileBond}, mol)
+
+    sdfilewriter(fname, [mg_mol])
+end
+
+function write_sdfile(fname::String, mols::AbstractArray{M}) where {M<:AbstractMolecule}
+    mg_mols = [convert(GraphMol{SDFileAtom, SDFileBond}, m) for m in mols]
+
+    sdfilewriter(fname, mg_mols)
 end
--- test/fileformats/test_sdfile.jl
+++ test/fileformats/test_sdfile.jl
@@ -1,37 +1,56 @@
 @testset "SDFile" begin
-    mols = load_sdfile("data/sdfile_test_1.sdf")
+    @testset "Reading" begin
+        mols = load_sdfile("data/sdfile_test_1.sdf")
 
-    @test length(mols) == 11
+        @test length(mols) == 11
 
-    @test sum(map(count_atoms, mols)) == 518
-    @test sum(map(count_bonds, mols)) == 528
+        @test sum(map(count_atoms, mols)) == 518
+        @test sum(map(count_bonds, mols)) == 528
 
-    mol = mols[1]
-    @test count_atoms(mol) == 39
-    @test count_bonds(mol) == 42
+        mol = mols[1]
+        @test count_atoms(mol) == 39
+        @test count_bonds(mol) == 42
 
-    @test has_property(mol, "NAME")
-    @test get_property(mol, "NAME") == "Abacavir_sulfate"
+        @test has_property(mol, "NAME")
+        @test get_property(mol, "NAME") == "Abacavir_sulfate"
 
-    @test has_property(mol, "b_1rotN")
-    @test get_property(mol, "b_1rotN") == "6"
+        @test has_property(mol, "b_1rotN")
+        @test get_property(mol, "b_1rotN") == "6"
 
-    @test has_property(mol, "Weight")
-    @test get_property(mol, "Weight") == "286.339"
+        @test has_property(mol, "Weight")
+        @test get_property(mol, "Weight") == "286.339"
 
-    @test has_property(mol, "TPSA")
-    @test get_property(mol, "TPSA") == "101.88"
+        @test has_property(mol, "TPSA")
+        @test get_property(mol, "TPSA") == "101.88"
 
-    @test has_property(mol, "a_acc")
-    @test get_property(mol, "a_acc") == "4"
+        @test has_property(mol, "a_acc")
+        @test get_property(mol, "a_acc") == "4"
 
-    @test has_property(mol, "a_don")
-    @test get_property(mol, "a_don") == "3"
+        @test has_property(mol, "a_don")
+        @test get_property(mol, "a_don") == "3"
 
-    @test has_property(mol, "logP(o/w)")
-    @test get_property(mol, "logP(o/w)") == "0.40906"
+        @test has_property(mol, "logP(o/w)")
+        @test get_property(mol, "logP(o/w)") == "0.40906"
 
-    @test has_property(mol, "SlogP")
-    @test get_property(mol, "SlogP") == "1.1878"
+        @test has_property(mol, "SlogP")
+        @test get_property(mol, "SlogP") == "1.1878"
+    end
     
+    @testset "Writing" begin
+        mols = load_sdfile("data/sdfile_test_1.sdf")
+
+        (single_name, single_file) = mktemp(;cleanup = true)
+
+        write_sdfile(single_name, mols[1])
+        m_sd = load_sdfile(single_name)[1]
+
+        @test m_sd == mols[1]
+
+        (set_name, set_file) = mktemp(;cleanup = true)
+
+        write_sdfile(set_name, mols)
+        ms_sd = load_sdfile(set_name)
+
+        @test ms_sd == mols
+    end
 end
