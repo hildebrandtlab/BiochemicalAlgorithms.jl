81072d3a41bbae7eca6760146ed8a7852e71d78b
Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>
2023-02-17 11:21:01 +0100

    Added BondOrder::Aromatic
    Signed-off-by: Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>

--- test/core/test_bond_order.jl
+++ test/core/test_bond_order.jl
@@ -3,6 +3,7 @@
     @test BondOrderType(2) == BondOrder.Double
     @test BondOrderType(3) == BondOrder.Triple
     @test BondOrderType(4) == BondOrder.Quadruple
+    @test BondOrderType(50) == BondOrder.Aromatic
     @test BondOrderType(100) == BondOrder.Unknown
     @test_throws ArgumentError BondOrderType(5)
     @test_throws ArgumentError BondOrderType(-1)
