35ed547d404e5d343ae9e4ff7796809776086c79
Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>
2023-02-17 11:20:52 +0100

    Added FragmentDB
    Signed-off-by: Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>

--- src/BiochemicalAlgorithms.jl
+++ src/BiochemicalAlgorithms.jl
@@ -30,6 +30,8 @@ include("fileformats/pdb.jl")
 include("mappings/atom_bijection.jl")
 include("mappings/rigid_mapping.jl")
 
+include("preprocessing/fragmentDB.jl")
+
 using .PubChem
 
 export load_pubchem_json, ball_data_path
