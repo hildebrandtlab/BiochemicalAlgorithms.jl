6ce7677a963af9141000d1ff55be82268f91f7eb
Thomas Kemmer <thomas@binaryden.de>
2023-02-23 18:04:55 +0100

    Adapt Protein to the AtomContainer interface
    Signed-off-by: Thomas Kemmer <thomas@binaryden.de>

--- src/core/protein.jl
+++ src/core/protein.jl
@@ -2,23 +2,53 @@ using DataFrames
 
 export Protein
 
+const _ProteinAtom{T} = NamedTuple{
+    (fieldnames(Atom{T})..., :frame_id, :residue_id),
+    Tuple{fieldtypes(Atom{T})..., Int, Int}
+}
+
 mutable struct Protein{T<:Real} <: AbstractMolecule{T}
     name::String
 
     atoms::DataFrame
     bonds::DataFrame
+    residues::DataFrame
+    properties::Properties
 
-    chains::Vector{ProteinChain}
-
-    function Protein{T}(name = "", 
-                        atoms = DataFrame(PDBAtom{T}[]), 
-                        bonds = DataFrame(Bond[]),
-                        chains = ProteinChain[]) where {T<:Real}
-        new(name, atoms, bonds, chains)
+    function Protein{T}(name = "") where T 
+        new(name, DataFrame(_ProteinAtom{T}[]), DataFrame(Bond[]), DataFrame(Residue[]), Properties())
     end
 end
 
-Protein(name="",
-        atoms=DataFrame(PDBAtom{Float32}[]),
-        bonds=DataFrame(Bond[]),
-        chains=ProteinChain[]) = Protein{Float32}(name, atoms, bonds, chains)
+Protein(name="") = Protein{Float32}(name)
+
+@inline function residues(ac::Protein{T}) where T
+    ac.residues
+end
+
+@inline function atoms(ac::Protein{T}; frame_id::Int = 1) where T
+    df = get(groupby(ac.atoms, :frame_id), (frame_id = frame_id,), DataFrame(_ProteinAtom{T}[]))
+    view(df, :, 1:length(fieldnames(Atom{T})))
+end
+
+@inline function atoms(ac::Protein{T}, r::Residue; frame_id::Int = 1) where T
+    df = get(
+        groupby(ac.atoms, [:frame_id, :residue_id]),
+        (frame_id = frame_id, residue_id = r.number),
+        DataFrame(_ProteinAtom{T}[])
+    )
+    view(df, :, 1:length(fieldnames(Atom{T})))
+end
+
+function Base.push!(ac::Protein{T}, r::Residue, atoms::Atom{T}...; frame_id::Int = 1) where T
+    # check whether the given residue is known, add it otherwise
+    res_df = ac.residues
+    if isempty(@view res_df[res_df.number .== r.number, :])
+        push!(res_df, r)
+    end
+
+    # add all atoms
+    for atom in atoms
+        push!(ac.atoms, (atom..., frame_id = frame_id, residue_id = r.number))
+    end
+end
--- test/test_protein.jl
+++ test/test_protein.jl
@@ -1,37 +1,65 @@
 @testset "Simple Protein" begin
 
-    protein = Protein("my_fancy_protein")
+    mol = Protein()
 
-    @test protein isa Protein
-    @test protein isa AbstractMolecule
-    @test !isa(protein, Molecule)
-    @test protein.name == "my_fancy_protein"
-    @test protein.atoms isa DataFrame
-    @test size(protein.atoms) == (0, 14)
-    @test protein.bonds isa DataFrame
-    @test size(protein.bonds) == (0, 4)
-    @test protein.chains isa Vector{ProteinChain}
-    @test size(protein.chains, 1) == 0
-    @test count_atoms(protein) == 0
-    @test count_bonds(protein) == 0
+    @test mol isa Protein{Float32}
+    @test mol isa AbstractMolecule{Float32}
+    @test name(mol) == ""
+    @test atoms(mol) isa SubDataFrame
+    @test size(atoms(mol)) == (0, 10)
+    @test bonds(mol) isa DataFrame
+    @test size(bonds(mol)) == (0, 4)
+    @test residues(mol) isa DataFrame
+    @test size(residues(mol), 1) == 0
+    @test count_atoms(mol) == 0
+    @test count_bonds(mol) == 0
+    @test length(properties(mol)) == 0
+    set_name!(mol, "my_fancy_protein")
+    @test name(mol) == "my_fancy_protein"
 end
 
 @testset "Filled Protein" begin
+    mol = Protein("my_fancy_molecule")
 
-    protein = Protein()
-    # now create PDBChains
-    chain_A = ProteinChain("chain A", 
-                            DataFrame(number = [1, 2, 3],
-                                      type = fill(AminoAcid('A'), 3),
-                                      chain = ["A", "A", "A"] ))
+    # add atoms
+    r_tmp = Vector3{Float32}(1.0, 2.0, 4.0)
+    atoms_df = DataFrame(number = [i for i in 1:6],
+                      name = fill("H", 6),
+                      element = fill(Elements.H, 6),
+                      atomtype = fill("na", 6),
+                      r = [i .* Vector3{Float32}(1.0, 2.0, 4.0) for i in 1:6],
+                      v = fill(Vector3{Float32}(0.0, 0.0, 0.0), 6), 
+                      F = fill(Vector3{Float32}(0.0, 0.0, 0.0), 6),
+                      has_velocity = fill(false, 6),
+                      has_force = fill(false, 6),
+                      properties = Properties()
+    )
+    r1 = (number = 1, type = AminoAcid('A'), chain = "A")::Residue
+    r2 = (number = 2, type = AminoAcid('D'), chain = "B")::Residue
+    for (i, atom) in enumerate(eachrow(atoms_df))
+        push!(mol, i < 4 ? r1 : r2, copy(atom)::Atom{Float32})
+    end
 
-    chain_B = ProteinChain("chain B", 
-                            DataFrame(number = [1, 2, 3],
-                                      type = fill(AminoAcid('D'), 3),
-                                      chain = ["B", "B", "B"] ))
-                                      
-    push!(protein.chains, chain_A)
-    push!(protein.chains, chain_B)
-    @test size(protein.chains, 1) == 2
-    @test protein.chains[1] isa ProteinChain
+    # add bonds
+    for i in 1:4
+        push!(mol, (a1 = i, a2 = 5, order = BondOrder.Single, properties = Properties())::Bond)
+    end
+
+    @test name(mol) == "my_fancy_molecule"
+    @test count_bonds(mol) == 4
+    @test count_atoms(mol) == 6
+    @test nrow(atoms(mol, r1)) == 3
+    @test nrow(atoms(mol, r2)) == 3
+
+    # test properties
+    props = properties(mol)
+    props["molecule computed"] = false
+    props["molecule resolution"] = 2.5
+    @test length(props) == 2
+    @test length(properties(mol)) == 2
+
+    props["resolution unit"] = "Angstroem"
+    @test length(properties(mol)) == 3
+    @test !properties(mol)["molecule computed"]
+    @test haskey(properties(mol), "resolution unit")
 end
