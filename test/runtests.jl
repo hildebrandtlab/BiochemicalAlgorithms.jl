3988d82c61f5ed692b70d9c21bfa5a57804c3354
Thomas Kemmer <thomas@binaryden.de>
2023-02-23 18:10:45 +0100

    Remove obsolete Chain types
    Signed-off-by: Thomas Kemmer <thomas@binaryden.de>

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,14 @@
 using BiochemicalAlgorithms
 using Test
+using DataFrames
 
 @testset "BiochemicalAlgorithms.jl" begin
    
-    @testset "Fileformats" begin include("test_fileformats.jl") end
+    @testset "File formats" begin include("test_fileformats.jl") end
     @testset "Core" begin 
         include("test_amino_acid.jl")
         include("test_atom.jl")
         include("test_bond.jl")
-        include("test_chain.jl")
         include("test_element.jl")
         include("test_fragment.jl")
         include("test_molecule.jl")
