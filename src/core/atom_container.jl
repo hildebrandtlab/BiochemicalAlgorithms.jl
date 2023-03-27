c46f66161f546887534e60d65b2b443056c3fff5
Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>
2023-03-27 15:46:59 +0200

    Add frame_ids() and nframes() to AbstractAtomContainer
    Signed-off-by: Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>
    Signed-off-by: Thomas Kemmer <thomas@binaryden.de>

--- src/core/atom_container.jl
+++ src/core/atom_container.jl
@@ -1,4 +1,4 @@
-export AbstractAtomContainer
+export AbstractAtomContainer, frame_ids, nframes
 
 """
     $(TYPEDEF)
@@ -6,3 +6,7 @@ export AbstractAtomContainer
 Abstract base type for all atom containers.
 """
 abstract type AbstractAtomContainer{T <: Real} end
+
+frame_ids(ac::AbstractAtomContainer{T}) where {T<:Real} = unique(_atoms(ac).frame_id)
+nframes(ac::AbstractAtomContainer{T}) where {T<:Real} = length(frame_ids(ac))
+
