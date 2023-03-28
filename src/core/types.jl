fd7f676170f39f3c5fcd1e3c320e5dbcb9fc9634
Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>
2023-03-28 10:21:41 +0200

    Add squared_norm and distance to Vector3
    Signed-off-by: Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>

--- src/core/types.jl
+++ src/core/types.jl
@@ -1,6 +1,6 @@
 using StaticArrays
 
-export Vector3, Matrix3, MaybeInt, Properties, Flags
+export Vector3, Matrix3, MaybeInt, Properties, Flags, squared_norm
 
 const Vector3{T} = SVector{3, T}
 const Matrix3{T} = SMatrix{3, 3, T}
@@ -8,3 +8,6 @@ const Matrix3{T} = SMatrix{3, 3, T}
 const MaybeInt = Union{Missing, Int}
 const Properties = Dict{Symbol, Any}
 const Flags = Set{Symbol}
+
+squared_norm(v::Vector3{T}) where {T<:Real} = dot(v, v)
+distance(v::Vector3{T}, w::Vector3{T}) where {T<:Real} = norm(v - w)
