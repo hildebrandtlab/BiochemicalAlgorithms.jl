d8324ae357033c2b7c671943d5e2bd7a1a9bed6d
Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>
2023-03-28 12:19:42 +0200

    Adapt Substructure to new core
    Signed-off-by: Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>

--- src/substructures/substructure.jl
+++ src/substructures/substructure.jl
@@ -4,8 +4,10 @@ export Substructure, filter_atoms
     name::String
 
     parent::AbstractAtomContainer{T}
-    atoms::SubDataFrame
-    bonds::SubDataFrame
+    
+    _atoms::SubDataFrame
+    _bonds::SubDataFrame
+    
     properties::Properties
 
     function Substructure{T}(name,
@@ -25,20 +27,79 @@ Substructure(name,
                 Substructure{Float32}(name, parent, atoms, bonds, properties)
 
 function filter_atoms(fn, mol; name="", adjacent_bonds=false)
-    atom_view = filter(fn, getfield(atoms_df(mol), :df), view=true)
+    atom_view = filter(fn, _atoms(mol), view=true)
     bond_view = filter(
         [:a1, :a2] => (a1, a2) -> 
             adjacent_bonds ? a1 ∈ atom_view.idx || a2 ∈ atom_view.idx
                            : a1 ∈ atom_view.idx && a2 ∈ atom_view.idx,
-        getfield(bonds_df(mol), :df), view=true)
+        _bonds(mol), view=true)
 
     Substructure(name, mol, atom_view, bond_view)
 end
 
-function atoms(ac::Substructure{T}) where {T<:Real}
-    collect(eachrow(ac.atoms))
+"""
+$(TYPEDSIGNATURES)
+
+Returns a raw `DataFrame` for all of the given system's atoms matching the given criteria (value or
+`missing`). Fields given as `nothing` are ignored. The returned `DataFrame` contains all public and
+private atom fields.
+"""
+function _atoms(substruct::Substructure{T};
+    frame_id::Union{Nothing, Int} = 1,
+    molecule_id::Union{Nothing, MaybeInt} = nothing,
+    chain_id::Union{Nothing, MaybeInt} = nothing,
+    fragment_id::Union{Nothing, MaybeInt} = nothing,
+    nucleotide_id::Union{Nothing, MaybeInt} = nothing,
+    residue_id::Union{Nothing, MaybeInt} = nothing
+) where T
+    cols = Tuple{Symbol, MaybeInt}[]
+    isnothing(frame_id)      || push!(cols, (:frame_id, frame_id))
+    isnothing(molecule_id)   || push!(cols, (:molecule_id, molecule_id))
+    isnothing(chain_id)      || push!(cols, (:chain_id, chain_id))
+    isnothing(fragment_id)   || push!(cols, (:fragment_id, fragment_id))
+    isnothing(nucleotide_id) || push!(cols, (:nucleotide_id, nucleotide_id))
+    isnothing(residue_id)    || push!(cols, (:residue_id, residue_id))
+
+    get(
+        groupby(substruct._atoms, getindex.(cols, 1)),
+        ntuple(i -> cols[i][2], length(cols)),
+        view(substruct._atoms, Int[], :)
+    )::SubDataFrame{DataFrame, DataFrames.Index, Vector{Int}}
 end
 
-function bonds(ac::Substructure{T}) where {T<:Real}
-    collect(eachrow(ac.bonds))
+function _bonds(substruct::Substructure; kwargs...)
+    aidx = _atoms(substruct; kwargs...).idx
+    @rsubset(
+        substruct._bonds, :a1 in aidx || :a2 in aidx; view = true
+    )::SubDataFrame{DataFrame, DataFrames.Index, <:AbstractVector{Int}}
+end
+
+@inline function eachatom(substruct::Substructure{T}; kwargs...) where T
+    sys = substruct.parent isa System{T} ? substruct.parent : parent_system(substruct.parent)
+    (Atom{T}(sys, row) for row in eachrow(_atoms(substruct; kwargs...)))
+end
+
+@inline function atoms(substruct::Substructure; kwargs...)
+    collect(eachatom(substruct; kwargs...))
+end
+
+@inline function eachbond(substruct::Substructure{T}; kwargs...) where T
+    sys = substruct.parent isa System{T} ? substruct.parent : parent_system(substruct.parent)
+    (Bond{T}(sys, row) for row in eachrow(_bonds(substruct; kwargs...)))
+end
+
+@inline function bonds(substruct::Substructure; kwargs...)
+    collect(eachbond(substruct; kwargs...))
+end
+
+function atoms_df(ac::Substructure{T}; kwargs...) where {T<:Real}
+    view(_atoms(ac; kwargs...), :, 1:length(fieldnames(AtomTuple{T})))
+end
+
+function bonds_df(ac::Substructure{T}; kwargs...) where {T<:Real}
+    view(_bonds(ac; kwargs...), :, 1:length(fieldnames(BondTuple{T})))
+end
+
+@inline function natoms(substruct::Substructure; kwargs...)
+    nrow(_atoms(substruct; kwargs...))
 end
