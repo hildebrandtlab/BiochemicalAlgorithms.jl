48741b0afb698bcd749e23160dc862e4ab48a949
Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>
2023-03-28 10:21:06 +0200

    Add has_property, get_property, and set_property to AbstractAtomContainer
    Signed-off-by: Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>

--- src/core/atom_container.jl
+++ src/core/atom_container.jl
@@ -1,4 +1,5 @@
-export AbstractAtomContainer, frame_ids, nframes
+export AbstractAtomContainer, frame_ids, nframes, 
+    has_property, get_property, set_property
 
 """
     $(TYPEDEF)
@@ -10,3 +11,19 @@ abstract type AbstractAtomContainer{T <: Real} end
 frame_ids(ac::AbstractAtomContainer{T}) where {T<:Real} = unique(_atoms(ac).frame_id)
 nframes(ac::AbstractAtomContainer{T}) where {T<:Real} = length(frame_ids(ac))
 
+
+function has_property(ac::AbstractAtomContainer{T}, key::String) where {T<:Real}
+    haskey(ac.properties, key)
+end
+
+function get_property(ac::AbstractAtomContainer{T}, key::String) where {T<:Real}
+    ac.properties[key]
+end
+
+function get_property(ac::AbstractAtomContainer{T}, key::String, default) where {T<:Real}
+    has_property(ac, key) ? ac.properties[key] : default
+end
+
+function set_property(ac::AbstractAtomContainer{T}, key::String, value) where {T<:Real}
+    ac.properties[key] = value
+end
