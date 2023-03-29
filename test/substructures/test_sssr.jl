0f069fcd1c5e70e74ecc98ed322867da8edb00c4
Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>
2023-03-29 15:53:24 +0200

    Add sssr tests.
    Signed-off-by: Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>

--- test/substructures/test_sssr.jl
+++ test/substructures/test_sssr.jl
@@ -3,7 +3,13 @@
 
     num_ring_atoms = [6, 6, 6, 6, 6, 13, 0, 6, 4]
 
-    detected_num_ring_atoms = map(length, map(is_ring_atom, molecules(system)))
-
+    detected_num_ring_atoms = map(sum, map(is_ring_atom, molecules(system)))
     @test num_ring_atoms == detected_num_ring_atoms
+
+    num_sssrs      = [1, 1, 1, 1, 1, 3, 0, 1, 3]
+    num_sssr_atoms = [[6], [6], [6], [6], [6], [6, 3, 6], [], [6], [3, 3, 3]]
+
+    detected_sssrs = map(find_sssr, molecules(system))
+    @test map(length, detected_sssrs) == num_sssrs
+    @test [map(length, sssr) for sssr in detected_sssrs] == num_sssr_atoms
 end
