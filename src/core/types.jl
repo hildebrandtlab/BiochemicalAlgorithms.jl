7accdb45775acacd5dc3bf9f9bcb9ddaef8e3330
Thomas Kemmer <thomas@binaryden.de>
2023-03-29 12:48:27 +0200

    Add Flags type
    Signed-off-by: Thomas Kemmer <thomas@binaryden.de>

--- src/core/types.jl
+++ src/core/types.jl
@@ -1,9 +1,10 @@
 using StaticArrays
 
-export Vector3, Matrix3, MaybeInt, Properties
+export Vector3, Matrix3, MaybeInt, Properties, Flags
 
 const Vector3{T} = SVector{3, T}
 const Matrix3{T} = SMatrix{3, 3, T}
 
 const MaybeInt = Union{Missing, Int}
 const Properties = Dict{String, Any}
+const Flags = Set{Symbol}
