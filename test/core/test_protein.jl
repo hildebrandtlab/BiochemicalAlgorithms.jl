a587bfff63c39dd0c95c55a4c674918dc0be18f8
Thomas Kemmer <thomas@binaryden.de>
2023-03-30 17:06:46 +0200

    Add Protein{T} tests
    Signed-off-by: Thomas Kemmer <thomas@binaryden.de>

--- test/core/test_protein.jl
+++ test/core/test_protein.jl
@@ -1,82 +1,143 @@
-@testset "Simple Protein" begin
+using BiochemicalAlgorithms: _proteins
 
-    mol = Protein()
+@testset "Protein" begin
+    for T in [Float32, Float64]
+        sys = System{T}()
 
-    @test mol isa Protein{Float32}
-    @test mol isa AbstractMolecule{Float32}
-    @test mol.name == ""
-    @test atoms_df(mol) isa AbstractDataFrame
-    @test size(atoms_df(mol)) == (0, 13)
-    @test length(atoms(mol)) == 0
-    @test bonds_df(mol) isa AbstractDataFrame
-    @test size(bonds_df(mol)) == (0, 6)
-    @test length(bonds(mol)) == 0
-    @test residues_df(mol) isa AbstractDataFrame
-    @test size(residues_df(mol), 1) == 0
-    @test length(residues(mol)) == 0
-    @test natoms(mol) == 0
-    @test nbonds(mol) == 0
-    @test length(mol.properties) == 0
-    mol.name = "my_fancy_protein"
-    @test mol.name == "my_fancy_protein"
-end
+        # constructors + parent
+        prot = Protein(sys)
+        @test prot isa Protein{T}
+        @test parent(prot) === sys
+        @test parent_system(prot) === sys
 
-@testset "Filled Protein" begin
-    mol = Protein("my_fancy_molecule")
+        if T == Float32
+            prot_ds = Protein()
+            @test parent(prot_ds) === default_system()
+            @test parent_system(prot_ds) === default_system()
 
-    # add atoms
-    r_tmp = Vector3{Float32}(1.0, 2.0, 4.0)
-    adf = DataFrame(
-        idx = fill(0, 6),
-        number = [i for i in 1:6],
-        element = fill(Elements.H, 6),
-        name = fill("H", 6),
-        atom_type = fill("na", 6),
-        r = [i .* Vector3{Float32}(1.0, 2.0, 4.0) for i in 1:6],
-        v = fill(Vector3{Float32}(0.0, 0.0, 0.0), 6), 
-        F = fill(Vector3{Float32}(0.0, 0.0, 0.0), 6),
-        formal_charge = 1,
-        charge = 2.0f32,
-        radius = 1.02f32,
-        properties = Properties(),
-        flags = Flags()
-    )
+            Protein("something", Properties(:a => "b"), Flags([:A]))
+        end
 
-    ca = Chain(mol, "A")
-    r1 = Residue(ca, 1, AminoAcid('A'))
-    cb = Chain(mol, "B")
-    r2 = Residue(cb, 2, AminoAcid('D'))
-    for (i, atom) in enumerate(eachrow(adf))
-        push!(i < 4 ? r1 : r2, copy(atom)::AtomTuple{Float32})
+        prot2 = Protein(sys, "something", Properties(:a => 1), Flags([:A, :B]))
+
+        #=
+            Make sure we test for the correct number of fields.
+            Add missing tests if the following test fails!
+        =#
+        @test length(getfield(prot, :_row)) == 4
+
+        # getproperty
+        @test prot.idx isa Int
+        @test prot.name isa String
+        @test prot.name == ""
+        @test prot.properties isa Properties
+        @test prot.properties == Properties()
+        @test prot.flags isa Flags
+        @test prot.flags == Flags()
+
+        @test prot._sys isa System{T}
+        @test prot._row isa DataFrameRow
+
+        @test prot2.name == "something"
+        @test prot2.properties == Properties(:a => 1)
+        @test prot2.flags == Flags([:A, :B])
+
+        # setproperty!
+        prot.name = "something else"
+        @test prot.name == "something else"
+        prot.properties = Properties(:first => "v1", :second => 99)
+        @test length(prot.properties) == 2
+        @test prot.properties[:first] == "v1"
+        @test prot.properties[:second] == 99
+        prot.flags = Flags([:C])
+        @test length(prot.flags) == 1
+        @test :C in prot.flags
+
+        # protein_by_idx
+        @test isnothing(protein_by_idx(sys, -1))
+        @test protein_by_idx(sys, prot.idx) isa Protein{T}
+        @test protein_by_idx(sys, prot.idx) == prot
+
+        # _proteins
+        df = _proteins(sys)
+        @test df isa AbstractDataFrame
+        @test size(df) == (2, length(fieldnames(ProteinTuple)))
+        @test copy(df[1, :]) isa ProteinTuple
+
+        # proteins_df
+        df = proteins_df(sys)
+        @test df isa AbstractDataFrame
+        @test size(df) == (2, length(fieldnames(ProteinTuple)))
+        @test copy(df[1, :]) isa ProteinTuple
+
+        # proteins
+        mv = proteins(sys)
+        @test mv isa Vector{Protein{T}}
+        @test length(mv) == 2
+
+        # eachprotein
+        @test first(eachprotein(sys)) isa Protein{T}
+        @test length(eachprotein(sys)) == 2
+
+        # nproteins
+        @test nproteins(sys) isa Int
+        @test nproteins(sys) == 2
+
+        # protein atoms
+        @test size(_atoms(prot), 1) == 0
+        @test _atoms(prot) == _atoms(sys, molecule_id = prot.idx)
+        @test size(atoms_df(prot), 1) == 0
+        @test atoms_df(prot) == atoms_df(sys, molecule_id = prot.idx)
+        @test length(atoms(prot)) == 0
+        @test atoms(prot) == atoms(sys, molecule_id = prot.idx)
+        @test length(eachatom(prot)) == 0
+        @test length(eachatom(prot)) == length(eachatom(sys, molecule_id = prot.idx))
+        @test natoms(prot) == 0
+        @test natoms(prot) == natoms(sys, molecule_id = prot.idx)
+
+        @test push!(prot, AtomTuple{T}(1, Elements.H)) === prot
+        @test size(_atoms(prot), 1) == 1
+        @test size(_atoms(prot)) == size(_atoms(sys, molecule_id = prot.idx))
+        @test size(atoms_df(prot), 1) == 1
+        @test size(atoms_df(prot)) == size(atoms_df(sys, molecule_id = prot.idx))
+        @test length(atoms(prot)) == 1
+        @test atoms(prot) == atoms(sys, molecule_id = prot.idx)
+        @test length(eachatom(prot)) == 1
+        @test length(eachatom(prot)) == length(eachatom(sys, molecule_id = prot.idx))
+        @test natoms(prot) == 1
+        @test natoms(prot) == natoms(sys, molecule_id = prot.idx)
+
+        for atom in eachatom(prot)
+            @test parent_protein(atom) === prot
+        end
+        @test parent_protein(Atom(prot, 2, Elements.C)) === prot
+
+        # protein bonds
+        @test size(_bonds(prot), 1) == 0
+        @test _bonds(prot) == _bonds(sys, molecule_id = prot.idx)
+        @test size(bonds_df(prot), 1) == 0
+        @test bonds_df(prot) == bonds_df(sys, molecule_id = prot.idx)
+        @test length(bonds(prot)) == 0
+        @test bonds(prot) == bonds(sys, molecule_id = prot.idx)
+        @test length(eachbond(prot)) == 0
+        @test length(eachbond(prot)) == length(eachbond(sys, molecule_id = prot.idx))
+        @test nbonds(prot) == 0
+        @test nbonds(prot) == nbonds(sys, molecule_id = prot.idx)
+
+        @test push!(prot, BondTuple(
+            Atom(prot, 1, Elements.H).idx,
+            Atom(prot, 2, Elements.C).idx,
+            BondOrder.Single)
+        ) === prot
+        @test size(_bonds(prot), 1) == 1
+        @test size(_bonds(prot)) == size(_bonds(sys, molecule_id = prot.idx))
+        @test size(bonds_df(prot), 1) == 1
+        @test size(bonds_df(prot)) == size(bonds_df(sys, molecule_id = prot.idx))
+        @test length(bonds(prot)) == 1
+        @test bonds(prot) == bonds(sys, molecule_id = prot.idx)
+        @test length(eachbond(prot)) == 1
+        @test length(eachbond(prot)) == length(eachbond(sys, molecule_id = prot.idx))
+        @test nbonds(prot) == 1
+        @test nbonds(prot) == nbonds(sys, molecule_id = prot.idx)
     end
-
-    # add bonds
-    aidx = Dict(a.number => a.idx for a in eachrow(atoms_df(mol)))
-    for i in 1:4
-        push!(mol, (
-            idx = 0,
-            a1 = aidx[i],
-            a2 = aidx[5],
-            order = BondOrder.Single,
-            properties = Properties(),
-            flags = Flags()
-        )::BondTuple)
-    end
-
-    @test mol.name == "my_fancy_molecule"
-    @test nbonds(mol) == 4
-    @test natoms(mol) == 6
-    @test natoms(r1) == 3
-    @test natoms(r2) == 3
-
-    # test properties
-    props = mol.properties
-    props[:molecule_computed] = false
-    props[:molecule_resolution] = 2.5
-    @test length(props) == 2
-
-    props[:resolution_unit] = "Angstroem"
-    @test length(props) == 3
-    @test !props[:molecule_computed]
-    @test haskey(props, :resolution_unit)
 end
