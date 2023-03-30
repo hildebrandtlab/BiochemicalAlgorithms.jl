eb1e241e1448e5f4b407b8fa8b80910df84a7183
Thomas Kemmer <thomas@binaryden.de>
2023-03-30 17:05:23 +0200

    Add ProteinTuple and parent_protein
    Protein and Molecule can now be used interchangeably (for the time
    being).
    
    Signed-off-by: Thomas Kemmer <thomas@binaryden.de>

--- src/core/protein.jl
+++ src/core/protein.jl
@@ -1,10 +1,13 @@
-export Protein, protein_by_idx, proteins, proteins_df, eachprotein, nproteins
+export Protein, ProteinTuple, protein_by_idx, proteins, proteins_df, eachprotein, nproteins,
+    parent_protein
 
 # TODO implement rules to distinguish from molecules
 const Protein{T} = Molecule{T}
+const ProteinTuple = MoleculeTuple
 _proteins(sys::System) = _molecules(sys)
 protein_by_idx(sys::System, idx::Int) = molecule_by_idx(sys, idx)
 proteins(sys::System) = molecules(sys)
 proteins_df(sys::System) = molecules_df(sys)
 eachprotein(sys::System) = eachmolecule(sys)
 nproteins(sys::System) = nmolecules(sys)
+parent_protein(ac::Union{Atom, Chain, Fragment, Nucleotide, Residue}) = parent_molecule(ac)
