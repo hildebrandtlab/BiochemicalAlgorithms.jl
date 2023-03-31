dff5c7761a921992b21ad54325cb969c46f41a68
Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>
2023-03-31 11:25:45 +0200

    Started implementation of AMBER
    Energies for bond stretches work.
    
    Signed-off-by: Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>
    
    Fix case of ini path for AmberFF
    
    Signed-off-by: Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>

---
+++ test/forcefields/AMBER/amberff_test.jl
@@ -0,0 +1,15 @@
+using BiochemicalAlgorithms
+
+@testitem "AmberFF" begin
+    fdb = FragmentDB()
+    p = load_pdb(ball_data_path("../test/data/AlaAla.pdb"))
+
+    normalize_names!(p, fdb)
+    build_bonds!(p, fdb)
+    reconstruct_fragments!(p, fdb)
+
+    a_ff = AmberFF(p)
+
+    @test compute_energy(a_ff) == 1.3630637f0
+    @test a_ff.energies[1] == 1.3630637f0
+end
