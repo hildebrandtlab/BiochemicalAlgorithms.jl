03efa383e0d7aa9ae08dc4cdeb17104eec3127d2
Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>
2023-03-31 11:17:46 +0200

    Added distance(a1::Atom, a2::Atom)
    Signed-off-by: Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>

--- src/core/types.jl
+++ src/core/types.jl
@@ -1,6 +1,6 @@
 using StaticArrays
 
-export Vector3, Matrix3, MaybeInt, Properties, Flags, squared_norm
+export Vector3, Matrix3, MaybeInt, Properties, Flags, squared_norm, distance
 
 const Vector3{T} = SVector{3, T}
 const Matrix3{T} = SMatrix{3, 3, T}
