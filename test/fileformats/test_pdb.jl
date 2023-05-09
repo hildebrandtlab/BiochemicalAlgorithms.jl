b4a964fa8b244a71e49933a642c224065c751f12
Thomas Kemmer <thomas@binaryden.de>
2023-05-09 13:24:17 +0200

    Add more load_pdb tests
    Signed-off-by: Thomas Kemmer <thomas@binaryden.de>

--- test/fileformats/test_pdb.jl
+++ test/fileformats/test_pdb.jl
@@ -1,16 +1,52 @@
+using BioStructures:
+    PDB,
+    countatoms,
+    countchains,
+    countresidues,
+    read
+
 @testset "PDB" begin
-    bpti = load_pdb("data/bpti.pdb")
+    sys = load_pdb("data/bpti.pdb")
+    @test sys.name == "bpti.pdb"
+    @test natoms(sys) == 454
+    @test nbonds(sys) == 0
+    @test nmolecules(sys) == 1
+    @test nchains(sys) == 1
+    @test nfragments(sys) == 58
+    @test nresidues(sys) == 0
+    @test nnucleotides(sys) == 0
 
-    @test bpti.name == "bpti.pdb"
+    sys = load_pdb("data/5PTI.pdb")
+    @test sys.name == "5PTI.pdb"
+    @test natoms(sys) == 1087
+    @test nbonds(sys) == 0
+    @test nmolecules(sys) == 1
+    @test nchains(sys) == 1
+    @test nfragments(sys) == 123
+    @test nresidues(sys) == 0
+    @test nnucleotides(sys) == 0
 
-    @test natoms(bpti) == 454
-    @test nbonds(bpti) == 0
-
-    pdb_5pti = load_pdb("data/5PTI.pdb")
-
-    @test pdb_5pti.name == "5PTI.pdb"
-
-    @test natoms(pdb_5pti) == 1087
-
-    @test nbonds(pdb_5pti) == 0
+    sys = load_pdb("data/2ptc.pdb")
+    orig = read("data/2ptc.pdb", PDB)
+    orig_model = orig.models[1]
+    @test sys.name == orig.name
+    @test natoms(sys) == countatoms(orig)
+    @test nbonds(sys) == 0
+    @test nmolecules(sys) == 1
+    @test nchains(sys) == countchains(orig)
+    @test nfragments(sys) == countresidues(orig)
+    for chain in eachchain(sys)
+        orig_chain = get(orig_model.chains, chain.name, nothing)
+        @test !isnothing(orig_chain)
+        @test natoms(chain) == countatoms(orig_chain)
+        @test nfragments(chain) == countresidues(orig_chain)
+        for frag in eachfragment(chain)
+            # reconstruct BioStructures naming for residues
+            orig_name = strip("$(get_property(frag, :is_hetero_fragment, false) ? "H_" : "")\
+                $(frag.number)$(get_property(frag, :insertion_code, ""))")
+            orig_frag = get(orig_chain.residues, orig_name, nothing)
+            @test !isnothing(orig_frag)
+            @test natoms(frag) == countatoms(orig_frag)
+        end
+    end
 end
