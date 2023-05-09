fa0bebf87007c3162fedc456f30a999b5108929a
Thomas Kemmer <thomas@binaryden.de>
2023-05-09 13:21:52 +0200

    load_pdb: read :is_hetero_fragment property from data
    This property is available as `het_res` from BioStructures.jl and used
    in the naming of residues there.
    
    Signed-off-by: Thomas Kemmer <thomas@binaryden.de>

--- src/fileformats/pdb.jl
+++ src/fileformats/pdb.jl
@@ -120,7 +120,10 @@ function Base.convert(::Type{System{T}}, orig_pdb::ProteinStructure) where {T<:R
     for orig_chain in collectchains(orig_pdb)
         chain = Chain(mol, orig_chain.id)
         for orig_frag in collectresidues(orig_chain)
-            Fragment(chain, orig_frag.number, orig_frag.name, Properties([:insertion_code => orig_frag.ins_code]))
+            Fragment(chain, orig_frag.number, orig_frag.name, Properties([
+                :is_hetero_fragment => orig_frag.het_res,
+                :insertion_code => orig_frag.ins_code
+            ]))
         end
     end
 
