118757a7bb904da1ee776c94c6d9839afe609526
Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>
2024-01-04 12:20:12 +0100

    Add Kabsch-Sander algorithm for backbone H-Bond prediction
    Signed-off-by: Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>

--- src/BiochemicalAlgorithms.jl
+++ src/BiochemicalAlgorithms.jl
@@ -96,6 +96,7 @@ include("preprocessing/normalize_names.jl")
 include("preprocessing/build_bonds.jl")
 include("preprocessing/add_hydrogens.jl")
 include("preprocessing/reconstruct_fragments.jl")
+include("preprocessing/predict_hbonds.jl")
 
 include("deprecation.jl")
 
