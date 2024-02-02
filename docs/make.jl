5417a4bfd6e77fc4973252b6fc868bda95be0e18
Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>
2024-02-02 16:59:56 +0100

    Build tutorials as part of make doc, and add to documentation
    This is based on a workflow proposed by Ronny Bergmann in https://forem.julialang.org/kellertuer/render-quarto-tutorials-in-documenterjl-with-github-actions-3fo
    
    Signed-off-by: Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,83 @@
+#!/usr/bin/env julia
+#
+#
+
+if "--help" ∈ ARGS
+    println(
+        """
+docs/make.jl
+
+Render the `BiochemicalAlgorithms.jl` documenation with optional arguments
+
+Arguments
+* `--exclude-tutorials` - exclude the tutorials from the menu of Documenter,
+  this can be used if you do not have Quarto installed to still be able to render the docs
+  locally on this machine. This option should not be set on CI.
+* `--help`              - print this help and exit without rendering the documentation
+* `--prettyurls`        – toggle the prettyurls part to true (which is otherwise only true on CI)
+* `--quarto`            – run the Quarto notebooks from the `tutorials/` folder before generating the documentation
+  this has to be run locally at least once for the `tutorials/*.md` files to exist that are included in
+  the documentation (see `--exclude-tutorials`) for the alternative.
+  If they are generated ones they are cached accordingly.
+  Then you can spare time in the rendering by not passing this argument.
+""",
+    )
+    exit(0)
+end
+
+#
+# (a) if docs is not the current active environment, switch to it
+# (from https://github.com/JuliaIO/HDF5.jl/pull/1020/) 
+if Base.active_project() != joinpath(@__DIR__, "Project.toml")
+    using Pkg
+    Pkg.activate(@__DIR__)
+    Pkg.develop(PackageSpec(; path=(@__DIR__) * "/../"))
+    Pkg.resolve()
+    Pkg.instantiate()
+end
+
+# (b) Did someone say render? Then we render!
+if "--quarto" ∈ ARGS
+    using CondaPkg
+    CondaPkg.withenv() do
+        @info "Rendering Quarto"
+        tutorials_folder = (@__DIR__) * "/../tutorials"
+        # instantiate the tutorials environment if necessary
+        Pkg.activate(tutorials_folder)
+        Pkg.resolve()
+        Pkg.instantiate()
+        Pkg.build("IJulia") # build IJulia to the right version.
+        Pkg.activate(@__DIR__) # but return to the docs one before
+        run(`quarto render $(tutorials_folder)`)
+    end
+end
+
+tutorials_in_menu = true
+if "--exclude-tutorials" ∈ ARGS
+    @warn """
+    You are excluding the tutorials from the Menu,
+    which might be done if you can not render them locally.
+
+    Remember that this should never be done on CI for the full documentation.
+    """
+    tutorials_in_menu = false
+end
+
+# (c) load necessary packages for the docs
 using BiochemicalAlgorithms
 using Documenter
 
 DocMeta.setdocmeta!(BiochemicalAlgorithms, :DocTestSetup, :(using BiochemicalAlgorithms); recursive=true)
 
+## Build tutorials menu
+tutorials_menu =
+    "How to..." => [
+        "get started" => "tutorials/getting_started.md",
+    ]
+
 const pages = Any[
     "Home" => "index.md",
+    (tutorials_in_menu ? [tutorials_menu] : [])...,
     "Library" => Any[
         "System representation" => "public/system.md",
         "Force fields" => "public/forcefields.md",
@@ -31,3 +104,6 @@ deploydocs(;
     repo="github.com/hildebrandtlab/BiochemicalAlgorithms.jl",
     devbranch="develop",
 )
+
+#back to main env
+Pkg.activate()
