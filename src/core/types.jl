3b6006f617bfa0d2e914d8829ecfb98bb53768ec
Thomas Kemmer <thomas@binaryden.de>
2024-02-22 15:45:47 +0100

    Introduce `RowProjectionVector{T}` type
    This is essentially a stripped-down version of the RowProjectionVector
    type from ImplicitArrays.jl
    
    Signed-off-by: Thomas Kemmer <thomas@binaryden.de>
    
    fx implicit arrays

--- src/core/types.jl
+++ src/core/types.jl
@@ -11,3 +11,21 @@ const Flags = Set{Symbol}
 
 squared_norm(v::Vector3{T}) where {T<:Real} = dot(v, v)
 distance(v::Vector3{T}, w::Vector3{T}) where {T<:Real} = norm(v - w)
+
+struct RowProjectionVector{T} <: AbstractArray{T, 1}
+    base::Vector{T}
+    rows::Vector{Int}
+end
+
+@inline Base.size(M::RowProjectionVector) = (length(M.rows),)
+
+@inline Base.getindex(
+    M::RowProjectionVector,
+    i::Int
+) = getindex(getproperty(M, :base), getindex(getproperty(M, :rows), i))
+
+@inline Base.setindex!(
+    M::RowProjectionVector{T},
+    v::T,
+    i::Int
+) where T = setindex!(getproperty(M, :base), v, getindex(getproperty(M, :rows), i))
