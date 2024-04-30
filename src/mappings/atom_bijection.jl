8295fe7fa7a30b01eeac17d81056ea2629266767
Jenny Leclaire <leclaire@uni-mainz.de>
2024-04-30 15:21:02 +0200

    added documentation
    Signed-off-by: Jenny Leclaire <leclaire@uni-mainz.de>

--- src/mappings/atom_bijection.jl
+++ src/mappings/atom_bijection.jl
@@ -1,7 +1,28 @@
 export AbstractAtomBijection, TrivialAtomBijection
+"""
+    $(TYPEDEF)
 
+Abstract base type for AtomBijections. 
+    
+"""
 abstract type AbstractAtomBijection{T} end
 
+"""
+    TrivialAtomBijection{T} <: AbstractAtomBijection{T}
+Mutable representation of a bijection of atoms based on the order of atoms in the individual atom containers, i.e., atom 1 of con
+
+# Public fields
+- `atoms_A::AtomTable{T}`
+- `atoms_B::AtomTable{T}`
+
+# Constructors
+`TrivialAtomBijection{T}(A::AbstractAtomContainer{T}, B::AbstractAtomContainer{T}) where T`
+
+Creates a new `TrivialAtomBijection{T}` with the atoms of the individual atom containers. 
+
+`TrivialAtomBijection{T}(atoms_A, B::AbstractAtomContainer{T}) where T`
+Creates a new `TrivialAtomBijection{T}`based on the unique set of atom_numbers of `atoms_A`.
+"""
 struct TrivialAtomBijection{T} <: AbstractAtomBijection{T}
     atoms_A::AtomTable{T}
     atoms_B::AtomTable{T}
@@ -17,5 +38,16 @@ struct TrivialAtomBijection{T} <: AbstractAtomBijection{T}
     end
 end
 
+"""
+    TrivialAtomBijection(A::AbstractAtomContainer, B::AbstractAtomContainer)
+
+Creates a new `TrivialAtomBijection{Float32}` as a default.
+"""
 TrivialAtomBijection(A::AbstractAtomContainer, B::AbstractAtomContainer) = TrivialAtomBijection{Float32}(A, B)
+
+"""
+    TrivialAtomBijection(A::AbstractAtomContainer, B::AbstractAtomContainer)
+
+Creates a new `TrivialAtomBijection{Float32}` based on the unique set of atom_numbers of `atoms_A`.
+"""
 TrivialAtomBijection(atoms_A, B::AbstractAtomContainer) = TrivialAtomBijection{Float32}(atoms_A, B)
