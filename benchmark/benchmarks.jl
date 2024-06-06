f8b83fdd23bb13980588825dc0692e5221f9ee4e
Jenny Leclaire <leclaire@uni-mainz.de>
2024-06-06 15:42:12 +0200

    created benchmarking suites for kernel creation, iteration and file formats
    Signed-off-by: Jenny Leclaire <leclaire@uni-mainz.de>

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -4,3 +4,6 @@ using BiochemicalAlgorithms
 const SUITE = BenchmarkGroup()
 
 include("AmberFF_bench.jl")
+include("KernelIteration_bench.jl")
+include("KernelCreation_bench.jl")
+include("FileFormats_bench.jl")
