1317eecb9f0e1a3debc3593de42b0f52ac95d3c2
Jenny Leclaire <leclaire@uni-mainz.de>
2024-07-05 12:34:34 +0200

    [fix] error in benchmarks
    Signed-off-by: Jenny Leclaire <leclaire@uni-mainz.de>

--- benchmark/KernelCreation_bench.jl
+++ benchmark/KernelCreation_bench.jl
@@ -2,19 +2,19 @@
 kernel_creation = SUITE["Kernel"]["Creation"] 
 
 sys = System()
-kernel_creation["AtomCreation"] = @benchmarkable Atom($sys,i, Elements.C)
+kernel_creation["AtomCreation"] = @benchmarkable Atom($sys, 1, Elements.C)
 
 
 sys1 = System()
 mol = Molecule(sys1)
 chain = Chain(mol)
-kernel_creation["FragmentCreation"] = @benchmarkable Fragment($chain,i)
+kernel_creation["FragmentCreation"] = @benchmarkable Fragment($chain,1)
 
 
 sys2 = System()
 mol = Molecule(sys2)
 chain2 = Chain(mol)
-kernel_creation["ResidueCreation"] = @benchmarkable Residue($chain, i, AminoAcid('D'))
+kernel_creation["ResidueCreation"] = @benchmarkable Residue($chain, 1, AminoAcid('D'))
 
 
 sys3 = System()
