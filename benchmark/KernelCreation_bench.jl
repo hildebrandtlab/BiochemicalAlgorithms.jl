fdec220769d45cafaf3535270bd9385a9da2d7ba
Jenny Leclaire <leclaire@uni-mainz.de>
2024-07-08 12:31:36 +0200

    increased sample sizes to match BALLs
    Signed-off-by: Jenny Leclaire <leclaire@uni-mainz.de>

--- benchmark/KernelCreation_bench.jl
+++ benchmark/KernelCreation_bench.jl
@@ -2,23 +2,23 @@
 kernel_creation = SUITE["Kernel"]["Creation"] 
 
 sys = System()
-kernel_creation["AtomCreation"] = @benchmarkable Atom($sys, 1, Elements.C)
+kernel_creation["AtomCreation"] = @benchmarkable Atom($sys, 1, Elements.C) samples = 1000000
 
 
 sys1 = System()
 mol = Molecule(sys1)
 chain = Chain(mol)
-kernel_creation["FragmentCreation"] = @benchmarkable Fragment($chain,1)
+kernel_creation["FragmentCreation"] = @benchmarkable Fragment($chain,1) samples = 1000000
 
 
 sys2 = System()
 mol = Molecule(sys2)
 chain2 = Chain(mol)
-kernel_creation["ResidueCreation"] = @benchmarkable Residue($chain, 1, AminoAcid('D'))
+kernel_creation["ResidueCreation"] = @benchmarkable Residue($chain, 1, AminoAcid('D')) samples = 1000000
 
 
 sys3 = System()
-kernel_creation["MoleculeCreation"] = @benchmarkable Molecule($sys3)
+kernel_creation["MoleculeCreation"] = @benchmarkable Molecule($sys3) samples = 1000000
 
 
-kernel_creation["SystemCreation"] = @benchmarkable System()
+kernel_creation["SystemCreation"] = @benchmarkable System() samples = 1000000
