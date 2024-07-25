a78e0c5c40c8809e1470f93e95d06739f7439834
Thomas Kemmer <thomas@binaryden.de>
2024-07-25 15:23:26 +0200

    System: fix `is_geminal` docstring
    Signed-off-by: Thomas Kemmer <thomas@binaryden.de>

--- src/core/atom.jl
+++ src/core/atom.jl
@@ -397,7 +397,7 @@ end
 """
     $(TYPEDSIGNATURES)
 
-    Decides if two atoms are geminal.
+Decides if two atoms are geminal.
     
 Two atoms are geminal if they do not share a common bond but both have a
 bond to a third atom. For example the two hydrogen atoms in water are geminal. 
