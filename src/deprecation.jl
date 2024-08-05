3a7ac33143c0743ecaf30b1181e25292036c3d48
Thomas Kemmer <thomas@binaryden.de>
2024-08-05 21:28:53 +0200

    Mappings: add deprecation warnings for removed/changed functions
    Signed-off-by: Thomas Kemmer <thomas@binaryden.de>

---
+++ src/deprecation.jl
@@ -0,0 +1,6 @@
+# removed in v0.3
+@deprecate RigidTransform{T}(r::RotMatrix3{T}, t::Vector3{T}) where {T <: Real} RigidTransform(r, t)
+@deprecate RigidTransform{T}(r::Matrix3{T}, t::Vector3{T}) where {T <: Real} RigidTransform(r, t)
+@deprecate TrivialAtomBijection{T}(A::AbstractAtomContainer{T}, B::AbstractAtomContainer{T}) where T TrivialAtomBijection(A, B)
+@deprecate TrivialAtomBijection{T}(A, B::AbstractAtomContainer{T}) where T TrivialAtomBijection(A, B)
+@deprecate compute_rmsd_minimizer(f::AbstractAtomBijection{T}, mini::Type{<: AbstractRMSDMinimizer}) where T compute_rmsd_minimizer(f; minimizer=mini)
