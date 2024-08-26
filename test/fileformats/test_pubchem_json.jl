fe2207ec20bab12766bc844987c5a9d77a879bea
Thomas Kemmer <thomas@binaryden.de>
2024-08-26 15:59:21 +0200

    PubChem: use basename as molecule name instead of full path
    Signed-off-by: Thomas Kemmer <thomas@binaryden.de>

--- test/fileformats/test_pubchem_json.jl
+++ test/fileformats/test_pubchem_json.jl
@@ -6,7 +6,7 @@
 
     mol = molecules(sys)[1]
     @test mol isa Molecule{Float32}
-    @test mol.name == ball_data_path("../test/data/aspirin_pug.json_2244")
+    @test mol.name == "aspirin_pug.json_2244"
     @test length(mol.properties) == 3
 
     @test natoms(mol) == 21
@@ -21,12 +21,12 @@
     @test length(sys2.properties) == 0
 
         # used for testing bond annotations as properties of bonds
-        values = ["BA_CROSSED", "BA_DASHED","BA_WAVY", "BA_DOTTED",       
-                  "BA_WEDGE_UP", "BA_WEDGE_DOWN", "BA_ARROW", "BA_AROMATIC",     
-                  "BA_RESONANCE", "BA_BOLD", "BA_FISCHER", "BA_CLOSECONTACT", 
+        values = ["BA_CROSSED", "BA_DASHED","BA_WAVY", "BA_DOTTED",
+                  "BA_WEDGE_UP", "BA_WEDGE_DOWN", "BA_ARROW", "BA_AROMATIC",
+                  "BA_RESONANCE", "BA_BOLD", "BA_FISCHER", "BA_CLOSECONTACT",
                   "BA_UNKNOWN", "BA_CROSSED", "BA_DASHED","BA_WAVY", "BA_DOTTED",
                   "BA_WEDGE_UP", "BA_WEDGE_DOWN", "BA_ARROW", "BA_AROMATIC"]
-     
+
         for (i, bond) in enumerate(bonds(sys2))
                 @test bond.properties[:PCBondAnnotation_for_conformer][1] == values[i]
                 @test bond.properties[:PCBondAnnotation_for_conformer][2] == values[i]
