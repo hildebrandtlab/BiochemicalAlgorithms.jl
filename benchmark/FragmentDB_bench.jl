9dbd488f88436978591e92c364225a082ee61dde
Jenny Leclaire <leclaire@uni-mainz.de>
2024-11-15 12:01:46 +0100

    added file with missing atoms and alternate naming scheme (manually curated) and fixed error in preparing molecule
    Signed-off-by: Jenny Leclaire <leclaire@uni-mainz.de>

--- benchmark/FragmentDB_bench.jl
+++ benchmark/FragmentDB_bench.jl
@@ -1,6 +1,5 @@
 function prepare_mol(fdb, steps)
-    sys = load_pdb(ball_data_path("../benchmark/data/aspirin_pug_bonds.json"))
-    #fdb = FragmentDB()
+    sys = load_pdb(ball_data_path("../benchmark/data/AmberFF_naming_del.pdb"))
 
     if steps > 0
         normalize_names!(sys, fdb)
@@ -19,14 +18,12 @@ end
 
 
 fdb_suite = SUITE["FragmentDB"]
-
 fdb_suite["Creation"] = @benchmarkable FragmentDB()
-
 
 fdb = FragmentDB()
 
-fdb_suite["Normalize_Name"] = @benchmarkable normalize_names!(sys, $fdb) setup=(sys = prepare_mol($fdb,1))
+fdb_suite["Normalize_Name"] = @benchmarkable normalize_names!(sys, $fdb) setup=(sys = prepare_mol($fdb,0))
 
-fdb_suite["Reconstruct_Fragments"] = @benchmarkable reconstruct_fragments!(sys, $fdb) setup=(sys = prepare_mol($fdb, 2))
+fdb_suite["Reconstruct_Fragments"] = @benchmarkable reconstruct_fragments!(sys, $fdb) setup=(sys = prepare_mol($fdb, 1))
 
-fdb_suite["Building_Bonds"] = @benchmarkable normalize_names!(sys, $fdb) (setup=(sys = prepare_mol($fdb, 3)))
+fdb_suite["Building_Bonds"] = @benchmarkable normalize_names!(sys, $fdb) (setup=(sys = prepare_mol($fdb, 2)))
