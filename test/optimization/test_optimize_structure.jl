ba333b547fe522c5bdeaa0dfef4c12c8a928141c
Thomas Kemmer <thomas@binaryden.de>
2024-12-04 16:46:41 +0100

    Optim: add simple tests
    Signed-off-by: Thomas Kemmer <thomas@binaryden.de>

---
+++ test/optimization/test_optimize_structure.jl
@@ -0,0 +1,27 @@
+@testitem "Optimize structure" begin
+    sys = load_pdb(ball_data_path("../test/data/AlaAla.pdb"))
+
+    fdb = FragmentDB()
+    normalize_names!(sys, fdb)
+    reconstruct_fragments!(sys, fdb)
+    build_bonds!(sys, fdb)
+
+    ff = AmberFF(sys)
+    @test compute_energy!(ff) ≈ 1425.5979f0
+    @test Int(optimize_structure!(ff).retcode) == 1
+    @test compute_energy!(ff) ≈ -374.3136f0
+end
+
+@testitem "Optimize hydrogen positions" begin
+    sys = load_pdb(ball_data_path("../test/data/AlaAla.pdb"))
+
+    fdb = FragmentDB()
+    normalize_names!(sys, fdb)
+    reconstruct_fragments!(sys, fdb)
+    build_bonds!(sys, fdb)
+
+    ff = AmberFF(sys)
+    @test compute_energy!(ff) ≈ 1425.5979f0
+    @test Int(optimize_hydrogen_positions!(ff).retcode) == 1
+    @test compute_energy!(ff) ≈ 1421.212f0
+end
