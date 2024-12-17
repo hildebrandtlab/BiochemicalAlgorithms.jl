b6d3169337ef00d55bcb87ecc23e14b69bd3b6fd
Thomas Kemmer <thomas@binaryden.de>
2024-12-17 15:37:20 +0100

    FF: allow paths to be passed as `AbstractString`s
    Signed-off-by: Thomas Kemmer <thomas@binaryden.de>

--- src/forcefields/AMBER/amberff_parameters.jl
+++ src/forcefields/AMBER/amberff_parameters.jl
@@ -5,9 +5,11 @@ export
     filename::String
     sections::OrderedDict{String, BALLIniFileSection}
 
-    function AmberFFParameters(filename::String = ball_data_path("forcefields/AMBER/amber96.ini"), T=Float32)
+    function AmberFFParameters(
+        filename::AbstractString = ball_data_path("forcefields/AMBER/amber96.ini"),
+        ::Type{T} = Float32
+    ) where {T <: Real}
         parameters_ini = read_ball_ini_file(filename, T)
-
         new(filename, parameters_ini.sections)
     end
 end
