f36698745b695e372c40be8c03849a98e7e4e15e
Jenny Leclaire <leclaire@uni-mainz.de>
2024-12-19 09:52:18 +0100

    [benchmarks] update number of samples

--- benchmark/FileFormats_bench.jl
+++ benchmark/FileFormats_bench.jl
@@ -1,14 +1,14 @@
 fileformat_pdb = SUITE["FileFormats"]["PDB"] 
 
-fileformat_pdb["Reading"] = @benchmarkable load_pdb(ball_data_path("../benchmark/data/AmberFF_bench.pdb")) 
+fileformat_pdb["Reading"] = @benchmarkable load_pdb(ball_data_path("../benchmark/data/AmberFF_bench.pdb")) samples = 1000
 
 
 fileformat_json = SUITE["FileFormats"]["Json"] 
-fileformat_json["Reading"] = @benchmarkable load_pubchem_json(ball_data_path("../benchmark/data/aspirin_pug_bonds.json")) 
+fileformat_json["Reading"] = @benchmarkable load_pubchem_json(ball_data_path("../benchmark/data/aspirin_pug_bonds.json")) samples = 1000
 
 
 fileformat_json = SUITE["FileFormats"]["SDfile"] 
-fileformat_json["Reading"] = @benchmarkable load_sdfile(ball_data_path("../test/data/sdfile_test_1.sdf"))
+fileformat_json["Reading"] = @benchmarkable load_sdfile(ball_data_path("../test/data/sdfile_test_1.sdf")) samples = 1000
 
 #s = ball_data_path("../test/data/sdfile_test_1.sdf")
 #fileformat_json["Writing"] = @benchmarkable write_sdfile(s, ball_data_path("../benchmark/data/sdf_out.sdf"))
--- benchmark/KernelIteration_bench.jl
+++ benchmark/KernelIteration_bench.jl
@@ -16,18 +16,18 @@ kernel_iteration = SUITE["Kernel"]["Iteration"]
 
 sys = generate_example()
 
-kernel_iteration["AtomIteration"]=  @benchmarkable for atom in atoms($sys) end
+kernel_iteration["AtomIteration"]=  @benchmarkable for atom in atoms($sys) end  samples = 1000000
 
-kernel_iteration["ResidueIteration"]=  @benchmarkable for res in residues($sys) end
+kernel_iteration["ResidueIteration"]=  @benchmarkable for res in residues($sys) end samples = 1000000
 
-kernel_iteration["ChainIteration"]=  @benchmarkable for chain in chains($sys) end
+kernel_iteration["ChainIteration"]=  @benchmarkable for chain in chains($sys) end samples = 1000000
 
-kernel_iteration["BondIteration"]=  @benchmarkable for bond in bonds($sys) end
+kernel_iteration["BondIteration"]=  @benchmarkable for bond in bonds($sys) end samples = 1000000
 
 
 kernel_clone = SUITE["Kernel"]["Clone"]
 sys2 = generate_example(false)
-kernel_clone["SystemCloning wo bonds"] = @benchmarkable s = deepcopy($sys2)
+kernel_clone["SystemCloning wo bonds"] = @benchmarkable s = deepcopy($sys2) samples = 1000000
 
 sys3 = generate_example(true)
-kernel_clone["SystemCloning with bonds"] = @benchmarkable s = deepcopy($sys3)
+kernel_clone["SystemCloning with bonds"] = @benchmarkable s = deepcopy($sys3) samples = 1000000
