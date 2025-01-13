fdf40d8aa4b8f60604fb0a2f2ca75e99ff096104
Thomas Kemmer <thomas@binaryden.de>
2025-01-13 14:52:04 +0100

    FileFormats: test all `load_`/`write_` functions for `Float{64,32}`
    Signed-off-by: Thomas Kemmer <thomas@binaryden.de>

--- test/fileformats/test_sdfile.jl
+++ test/fileformats/test_sdfile.jl
@@ -1,28 +1,32 @@
-@testitem "SDFile Reading" begin
-    mols = molecules(load_sdfile(ball_data_path("../test/data/sdfile_test_1.sdf")))
+@testitem "Read SDFile" begin
+    for T in [Float32, Float64]
+        sys = load_sdfile(ball_data_path("../test/data/sdfile_test_1.sdf"), T)
+        mols = molecules(sys)
 
-    @test length(mols) == 11
+        @test sys isa System{T}
+        @test length(mols) == 11
 
-    @test sum(map(natoms, mols)) == 518
-    @test sum(map(nbonds, mols)) == 528
+        @test sum(map(natoms, mols)) == 518
+        @test sum(map(nbonds, mols)) == 528
 
-    mol = mols[1]
-    @test natoms(mol) == 39
-    @test nbonds(mol) == 42
+        mol = mols[1]
+        @test natoms(mol) == 39
+        @test nbonds(mol) == 42
 
-    @test has_property(mol, :metadata)
-    met = get_property(mol, :metadata)
-    @test met["NAME"] == "Abacavir_sulfate"
-    @test met["b_1rotN"] == "6"
-    @test met["Weight"] == "286.339"
-    @test met["TPSA"] == "101.88"
-    @test met["a_acc"] == "4"
-    @test met["a_don"] == "3"
-    @test met["logP(o/w)"] == "0.40906"
-    @test met["SlogP"] == "1.1878"
+        @test has_property(mol, :metadata)
+        met = get_property(mol, :metadata)
+        @test met["NAME"] == "Abacavir_sulfate"
+        @test met["b_1rotN"] == "6"
+        @test met["Weight"] == "286.339"
+        @test met["TPSA"] == "101.88"
+        @test met["a_acc"] == "4"
+        @test met["a_don"] == "3"
+        @test met["logP(o/w)"] == "0.40906"
+        @test met["SlogP"] == "1.1878"
+    end
 end
 
-@testitem "SDFile Writing" begin
+@testitem "Write SDFile" begin
     using DataFrames
 
     @inline function _compare_without_system(m1::AbstractAtomContainer, m2::AbstractAtomContainer)
@@ -31,29 +35,31 @@ end
             DataFrame(bonds(m1))  == DataFrame(bonds(m2)) &&
             m1.properties == m2.properties
     end
-    
-    sys = load_sdfile(ball_data_path("../test/data/sdfile_test_1.sdf"))
-    mols = molecules(sys)
 
-    (single_name, single_file) = mktemp(;cleanup = true)
+    for T in [Float32, Float64]
+        sys = load_sdfile(ball_data_path("../test/data/sdfile_test_1.sdf"), T)
+        mols = molecules(sys)
 
-    write_sdfile(single_name, first(mols))
-    m_sd = first(molecules(load_sdfile(single_name)))
+        (single_name, single_file) = mktemp(;cleanup = true)
 
-    # set third component of all positions to mimic `write_sdfile` behavior
-    atoms(sys).r .= map(r -> Vector3{Float32}(r[1], r[2], zero(Float32)), atoms(sys).r)
+        write_sdfile(single_name, first(mols))
+        m_sd = first(molecules(load_sdfile(single_name, T)))
 
-    # since the molecules have different systems, we cannot simply compare them directly
-    # also, m_sd contains the atom_idx property due to the GraphMol conversion
-    m_sd.properties = filter(((k,v),) -> k != :atom_idx, m_sd.properties)
-    @test _compare_without_system(m_sd, first(mols))
+        # zero third component of all positions to mimic `write_sdfile` behavior
+        atoms(sys).r .= map(r -> Vector3{T}(r[1], r[2], zero(T)), atoms(sys).r)
 
-    (set_name, set_file) = mktemp(;cleanup = true)
+        # since the molecules have different systems, we cannot simply compare them directly
+        # also, m_sd contains the atom_idx property due to the GraphMol conversion
+        m_sd.properties = filter(((k,v),) -> k != :atom_idx, m_sd.properties)
+        @test _compare_without_system(m_sd, first(mols))
 
-    write_sdfile(set_name, sys)
-    ms_sd = molecules(load_sdfile(set_name))
+        (set_name, set_file) = mktemp(; cleanup = true)
 
-    ms_sd = map(m -> begin m.properties = filter(((k,v),) -> k != :atom_idx, m.properties); m end, ms_sd)
+        write_sdfile(set_name, sys)
+        ms_sd = molecules(load_sdfile(set_name, T))
 
-    @test all([_compare_without_system(ms_sd[i], mols[i]) for i in eachindex(ms_sd)])
+        ms_sd = map(m -> begin m.properties = filter(((k,v),) -> k != :atom_idx, m.properties); m end, ms_sd)
+
+        @test all([_compare_without_system(ms_sd[i], mols[i]) for i in eachindex(ms_sd)])
+    end
 end
