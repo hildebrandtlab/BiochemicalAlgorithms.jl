136b0480f0ef483ed0b736f12a288ace3cf8cec4
Thomas Kemmer <thomas@binaryden.de>
2025-11-18 23:54:02 +0100

    Test: exclude hotfixed IntervalSets from stale deps check
    Signed-off-by: Thomas Kemmer <thomas@binaryden.de>

--- test/aqua.jl
+++ test/aqua.jl
@@ -1,5 +1,7 @@
 @testitem "Aqua" begin
     using Aqua
 
-    Aqua.test_all(BiochemicalAlgorithms)
+    # Ignore hotfixed package IntervalSets (https://github.com/hildebrandtlab/BiochemicalAlgorithms.jl/pull/221)
+    # for downgrade checks where the package is not pulled in (e.g. via MakieCore)
+    Aqua.test_all(BiochemicalAlgorithms; stale_deps = (; ignore = [:IntervalSets]))
 end
