a07e22c808237c7dde2e2c04f850d63bfb3f9f20
Thomas Kemmer <thomas@binaryden.de>
2026-02-27 11:54:19 +0100

    FragmentDB: add `ReconstructFragmentProcessor_test` tests from BALL
    Signed-off-by: Thomas Kemmer <thomas@binaryden.de>

--- test/preprocessing/test_reconstruct_fragments.jl
+++ test/preprocessing/test_reconstruct_fragments.jl
@@ -1,17 +1,25 @@
-@testitem "reconstruct_fragments!()" begin
+@testitem "reconstruct_fragments!" begin
     for T in [Float32, Float64]
         fdb = FragmentDB{T}()
 
         # single residue
-        s = System{T}()
-        f = Fragment(Chain(Molecule(s)), 1; name="LYS")
+        let sys = System{T}()
+            f = Fragment(Chain(Molecule(sys)), 1; name="LYS")
 
-        a = Atom(f, 1, Elements.C; name="CA")
+            a = Atom(f, 1, Elements.C; name="CA")
 
-        num_reconstructed = reconstruct_fragments!(s, fdb)
+            num_reconstructed = reconstruct_fragments!(sys, fdb)
 
-        @test num_reconstructed == natoms(s) - 1
-        @test natoms(f) == length(fdb.fragments["LYS"].variants[1].atoms)
-        @test natoms(f) == 22
+            @test num_reconstructed == natoms(sys) - 1
+            @test natoms(f) == length(fdb.fragments["LYS"].variants[1].atoms)
+            @test natoms(f) == 22
+        end
+
+        let sys = load_hinfile(ball_data_path("../test/data/ReconstructFragmentProcessor_test1.hin"), T)
+            normalize_names!(sys, fdb)
+
+            @test reconstruct_fragments!(sys, fdb) == 4
+            @test natoms(sys) == 31
+        end
     end
 end
