fc14bef954918c9239c533c85e7099a97ed5ccf8
Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>
2026-02-28 11:34:05 +0100

    Extend tests for add_hydrogens
    Signed-off-by: Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>

--- test/preprocessing/test_add_hydrogens.jl
+++ test/preprocessing/test_add_hydrogens.jl
@@ -1,11 +1,55 @@
 @testitem "add_hydrogens!" begin
     for T in [Float32, Float64]
-        sys = load_pdb(ball_data_path("../test/data/AlaAla.pdb"), T)
         fdb = FragmentDB{T}()
-        normalize_names!(sys, fdb)
-        reconstruct_fragments!(sys, fdb)
-        build_bonds!(sys, fdb)
 
-        @test add_hydrogens!(sys) == 1
+        # AlaAla dipeptide: adds 1 peptide bond hydrogen
+        let sys = load_pdb(ball_data_path("../test/data/AlaAla.pdb"), T)
+            normalize_names!(sys, fdb)
+            reconstruct_fragments!(sys, fdb)
+            build_bonds!(sys, fdb)
+
+            before = natoms(sys)
+            @test add_hydrogens!(sys) == 1
+            @test natoms(sys) == before + 1
+
+            # verify the added atom is a hydrogen
+            added = atoms(sys)[end]
+            @test added.element == Elements.H
+        end
+
+        # AlaGlySer tripeptide: adds peptide bond hydrogens
+        let sys = load_hinfile(ball_data_path("../test/data/AlaGlySer.hin"), T)
+            normalize_names!(sys, fdb)
+            reconstruct_fragments!(sys, fdb)
+            build_bonds!(sys, fdb)
+
+            @test add_hydrogens!(sys) == 1
+        end
+
+        # bpti: adds peptide bond hydrogen
+        let sys = load_pdb(ball_data_path("../test/data/bpti.pdb"), T)
+            normalize_names!(sys, fdb)
+            reconstruct_fragments!(sys, fdb)
+            build_bonds!(sys, fdb)
+
+            @test add_hydrogens!(sys) == 1
+        end
+
+        # already-saturated system: second pass adds 0
+        let sys = load_pdb(ball_data_path("../test/data/AlaAla.pdb"), T)
+            normalize_names!(sys, fdb)
+            reconstruct_fragments!(sys, fdb)
+            build_bonds!(sys, fdb)
+
+            add_hydrogens!(sys)
+            n = natoms(sys)
+            @test add_hydrogens!(sys) == 0
+            @test natoms(sys) == n
+        end
+
+        # empty system: no crash
+        let sys = System{T}()
+            @test add_hydrogens!(sys) == 0
+        end
     end
 end
