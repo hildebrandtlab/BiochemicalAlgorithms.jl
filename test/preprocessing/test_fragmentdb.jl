2a4ce678dfef7b85a0262c864dac1731741bb967
Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>
2026-02-28 11:34:28 +0100

    extend tests for fragmentdb
    Signed-off-by: Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>
    Signed-off-by: Thomas Kemmer <thomas@binaryden.de>

--- test/preprocessing/test_fragmentdb.jl
+++ test/preprocessing/test_fragmentdb.jl
@@ -1,11 +1,56 @@
 @testitem "FragmentDB" begin
-    default_fdb = FragmentDB()
+    for fdb in (FragmentDB(), FragmentDB{Float32}(), FragmentDB{Float64}())
 
-    @test length(default_fdb.fragments) == 33
-    @test length(default_fdb.name_mappings) == 6
-    @test length(default_fdb.defaults) == 1
+        @test length(fdb.fragments) == 33
+        @test length(fdb.name_mappings) == 6
+        @test length(fdb.defaults) == 1
+        @test fdb.defaults["Naming"] == "PDB"
+    end
 
-    fdb = FragmentDB(ball_data_path("fragments/Fragments.db.json"))
+    for T in [Float32, Float64]
+        # explicit path gives the same result
+        fdb = FragmentDB{T}()
+        fdb2 = FragmentDB{T}(ball_data_path("fragments/Fragments.db.json"))
+        @test fdb == fdb2
 
-    @test fdb == default_fdb
+        # GLY reference fragment
+        let gly = fdb.fragments["GLY"]
+            @test length(gly.variants) == 4
+            @test length(gly.atoms) == 10
+            @test length(gly.bonds) == 9
+        end
+
+        # ALA reference fragment
+        let ala = fdb.fragments["ALA"]
+            @test length(ala.variants) == 4
+            @test length(ala.atoms) == 13
+            @test length(ala.bonds) == 12
+        end
+
+        # LYS reference fragment
+        let lys = fdb.fragments["LYS"]
+            @test length(lys.variants) == 4
+            @test length(lys.atoms) == 25
+            @test length(lys.bonds) == 24
+        end
+
+        # get_reference_fragment: known fragment returns variant
+        let sys = System{T}()
+            f = Fragment(Chain(Molecule(sys)), 1; name="GLY")
+            ref = get_reference_fragment(f, fdb)
+            @test !isnothing(ref)
+            @test ref.name == "Default"
+            @test length(ref.atoms) == 7
+            @test length(ref.bonds) == 6
+        end
+
+        # get_reference_fragment: unknown fragment returns nothing
+        let sys = System{T}()
+            f = Fragment(Chain(Molecule(sys)), 1; name="UNKNOWN")
+            @test isnothing(get_reference_fragment(f, fdb))
+        end
+
+        # show method
+        @test contains(repr(fdb), "33 fragments")
+    end
 end
