bd959f9e4e0498a324a3c1376486a50030e67b23
Thomas Kemmer <thomas@binaryden.de>
2026-03-03 13:21:27 +0100

    Apply suggestions from code review
    Signed-off-by: Thomas Kemmer <thomas@binaryden.de>

--- test/preprocessing/test_add_hydrogens.jl
+++ test/preprocessing/test_add_hydrogens.jl
@@ -2,7 +2,7 @@
     for T in [Float32, Float64]
         fdb = FragmentDB{T}()
 
-        # AlaAla dipeptide: adds 1 peptide bond hydrogen
+        # AlaAla dipeptide: adds terminal OXT hydrogen
         let sys = load_pdb(ball_data_path("../test/data/AlaAla.pdb"), T)
             normalize_names!(sys, fdb)
             reconstruct_fragments!(sys, fdb)
@@ -13,26 +13,29 @@
             @test natoms(sys) == before + 1
 
             # verify the added atom is a hydrogen
-            added = atoms(sys)[end]
+            added = last(atoms(sys))
             @test added.element == Elements.H
+            @test added.name == "HOXT"
         end
 
-        # AlaGlySer tripeptide: adds peptide bond hydrogens
+        # AlaGlySer tripeptide: adds terminal OXT hydrogen
         let sys = load_hinfile(ball_data_path("../test/data/AlaGlySer.hin"), T)
             normalize_names!(sys, fdb)
             reconstruct_fragments!(sys, fdb)
             build_bonds!(sys, fdb)
 
             @test add_hydrogens!(sys) == 1
+            @test last(atoms(sys)).name == "HOXT"
         end
 
-        # bpti: adds peptide bond hydrogen
+        # bpti: adds terminal OXT hydrogen
         let sys = load_pdb(ball_data_path("../test/data/bpti.pdb"), T)
             normalize_names!(sys, fdb)
             reconstruct_fragments!(sys, fdb)
             build_bonds!(sys, fdb)
 
             @test add_hydrogens!(sys) == 1
+            @test last(atoms(sys)).name == "HOXT"
         end
 
         # already-saturated system: second pass adds 0
