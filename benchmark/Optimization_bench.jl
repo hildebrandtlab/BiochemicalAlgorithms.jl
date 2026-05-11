895ff4c5a98de987a646b2a6749b927f5084e338
Jenny Leclaire <leclaire@uni-mainz.de>
2026-05-11 10:17:06 +0200

    add minimial bench example for optimization
    Signed-off-by: Jenny Leclaire <leclaire@uni-mainz.de>

---
+++ benchmark/Optimization_bench.jl
@@ -0,0 +1,15 @@
+function setup_optimization()
+    sys = load_pdb(ball_data_path("../test/data/5PTI.pdb"))
+    infer_topology!(sys)
+    AmberFF(sys)
+end
+
+optimization_suite = SUITE["Optimization"]
+
+optimization_suite["optimize_structure!"] = @benchmarkable begin
+    optimize_structure!(ff; maxiters=10)
+end (setup = (ff = setup_optimization()))
+
+optimization_suite["optimize_hydrogen_positions!"] = @benchmarkable begin
+    optimize_hydrogen_positions!(ff; maxiters=10)
+end (setup = (ff = setup_optimization()))
