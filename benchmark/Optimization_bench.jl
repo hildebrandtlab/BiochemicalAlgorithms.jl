9d2eda5f2de755112c093b07c31de2b13bbbb90c
Jenny Leclaire <leclaire@uni-mainz.de>
2026-07-06 11:43:05 +0200

    [bench] added case for minibatching
    Signed-off-by: Jenny Leclaire <leclaire@uni-mainz.de>

--- benchmark/Optimization_bench.jl
+++ benchmark/Optimization_bench.jl
@@ -1,5 +1,5 @@
 function setup_optimization()
-    sys = load_pdb(ball_data_path("../test/data/5PTI.pdb"))
+    sys = load_pdb(ball_data_path("../test/data/AlaAla.pdb"))
     infer_topology!(sys)
     AmberFF(sys)
 end
@@ -8,6 +8,10 @@ optimization_suite = SUITE["Optimization"]
 
 optimization_suite["optimize_structure!"] = @benchmarkable begin
     optimize_structure!(ff; maxiters=10)
+end (setup = (ff = setup_optimization()))
+
+optimization_suite["optimize_structure_mini!"] = @benchmarkable begin
+    optimize_structure_mini!(ff; epochs=100, batchsize=10)
 end (setup = (ff = setup_optimization()))
 
 optimization_suite["optimize_hydrogen_positions!"] = @benchmarkable begin
