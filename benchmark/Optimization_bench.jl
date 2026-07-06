0989f5b891f8d7ec12559042d5b7bdda61bbaeab
Jenny Leclaire <leclaire@uni-mainz.de>
2026-07-06 11:43:05 +0200

    [bench] added case for minibatching
    Signed-off-by: Jenny Leclaire <leclaire@uni-mainz.de>

--- benchmark/Optimization_bench.jl
+++ benchmark/Optimization_bench.jl
@@ -10,6 +10,10 @@ optimization_suite["optimize_structure!"] = @benchmarkable begin
     optimize_structure!(ff; maxiters=10)
 end (setup = (ff = setup_optimization()))
 
+optimization_suite["optimize_structure_mini!"] = @benchmarkable begin
+    optimize_structure_mini!(ff; epochs=100, batchsize=10)
+end (setup = (ff = setup_optimization()))
+
 optimization_suite["optimize_hydrogen_positions!"] = @benchmarkable begin
     optimize_hydrogen_positions!(ff; maxiters=10)
 end (setup = (ff = setup_optimization()))
