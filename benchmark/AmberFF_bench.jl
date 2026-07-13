986fc945e1bd8ab7d6e075822cfbf3dc24ed5e33
Jenny Leclaire <leclaire@uni-mainz.de>
2026-07-13 15:05:06 +0200

    [cff]benchmarks
    Signed-off-by: Jenny Leclaire <leclaire@uni-mainz.de>

--- benchmark/AmberFF_bench.jl
+++ benchmark/AmberFF_bench.jl
@@ -35,3 +35,36 @@ amber_suite["compute_forces!(::ForceField)"] = @benchmarkable compute_forces!($a
 for i in a_ff.components
     amber_suite["compute_energy!(::$(i.name))"] = @benchmarkable compute_energy!($i)
 end
+
+# Compiled ForceField benchmarks
+cff_suite = SUITE["ForceFields"]["CompiledForceField"]
+
+cff_suite["Creation"] = @benchmarkable compile(a_ff; acc=Float32) (setup=(a_ff = AmberFF(prepare_mol("AmberFF_bench.pdb")); setup!(a_ff); update!(a_ff)))
+
+cff = compile(a_ff; acc=Float32)
+
+cff_suite["compute_energy!(::CompiledForceField)"] = @benchmarkable compute_energy!($cff)
+cff_suite["compute_forces!(::CompiledForceField)"] = @benchmarkable compute_forces!($cff)
+
+# Component-level benchmarks for CompiledForceField
+cff_suite["_energy_stretch"] = @benchmarkable BiochemicalAlgorithms._energy_stretch($cff)
+cff_suite["_energy_bend"] = @benchmarkable BiochemicalAlgorithms._energy_bend($cff)
+cff_suite["_energy_torsion"] = @benchmarkable BiochemicalAlgorithms._energy_torsion($cff, $cff.proper)
+cff_suite["_energy_improper"] = @benchmarkable BiochemicalAlgorithms._energy_torsion($cff, $cff.improper)
+
+cff_suite["_sum_lj"] = @benchmarkable BiochemicalAlgorithms._sum_lj($cff.r, $cff.lj, $cff.vdw_sw, 1, length($cff.lj.i))
+cff_suite["_sum_hb"] = @benchmarkable BiochemicalAlgorithms._sum_hb($cff.r, $cff.hb, $cff.vdw_sw, 1, length($cff.hb.i))
+cff_suite["_sum_es"] = @benchmarkable BiochemicalAlgorithms. _sum_es($cff.r, $cff.es, $cff.es_sw, $cff.distance_dependent_dielectric, $cff.es_prefactor, 1, length($cff.es.i))
+
+
+# Comparison benchmarks: reference vs compiled (Float32 acc)
+comparison_suite = SUITE["ForceFields"]["Comparison"]
+
+comparison_suite["Energy: AmberFF vs CompiledForceField (Float32)"] = @benchmarkable (compute_energy!($a_ff), compute_energy!($cff))
+comparison_suite["Forces: AmberFF vs CompiledForceField (Float32)"] = @benchmarkable (compute_forces!($a_ff), compute_forces!($cff))
+
+# Comparison benchmarks: reference vs compiled (Float64 acc)
+cff64 = compile(a_ff; acc=Float64)
+
+comparison_suite["Energy: AmberFF vs CompiledForceField (Float64)"] = @benchmarkable (compute_energy!($a_ff), compute_energy!($cff64))
+comparison_suite["Forces: AmberFF vs CompiledForceField (Float64)"] = @benchmarkable (compute_forces!($a_ff), compute_forces!($cff64))
